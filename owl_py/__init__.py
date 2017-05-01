6e2f36ccd2802c7d11b12cec9e51684c747d3a0f
Mateusz Zwierzycki <mateuszzwierzycki@live.com>
2017-05-01 19:29:03 +0200

    random

---
+++ owl_py/__init__.py
@@ -0,0 +1,1 @@
+from . import communication_utils, idx_numpy, owl_data_types, tbin_numpy
--- owl_py/owl_data_types.py
+++ owl_py/owl_data_types.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import random
+
 import numpy as np
 
 
@@ -14,6 +16,7 @@ class TensorSet:
         self._test = test
         self._validate = validate
         self._current_train = 0
+        self._seed = 123
 
     @property
     def tensors(self):
@@ -31,6 +34,34 @@ class TensorSet:
     def validate(self):
         return self._validate
 
+    @property
+    def seed(self):
+        return self._seed
+
+    # this is very very crude, needs some more work to be actually useful
+
+    def next_batch_shuffled(self, batch_size, shuffle_seed=-1):
+        this_batch = []
+
+        for i in range(batch_size):
+            index = int((self._current_train + i + self.train) % self.train)
+            this_batch.append(self.tensors[index])
+
+        self._seed = random.randint(0, 10000)
+        if shuffle_seed == -1: shuffle_seed = self._seed
+
+        random.seed(shuffle_seed)
+        random.shuffle(this_batch)
+
+        self._current_train += batch_size
+        if self._current_train > self.tensors.shape[0]: self._current_train = 0
+
+        shp = [batch_size, -1]
+
+        this_batch = np.vstack(this_batch)
+        this_batch = this_batch.reshape(shp)
+        return this_batch
+
     def next_batch(self, batch_size):
         this_batch = []
 
@@ -41,5 +72,8 @@ class TensorSet:
         self._current_train += batch_size
         if self._current_train > self.tensors.shape[0] : self._current_train = 0
 
+        shp = [batch_size, -1]
+
         this_batch = np.vstack(this_batch)
+        this_batch = this_batch.reshape(shp)
         return this_batch
