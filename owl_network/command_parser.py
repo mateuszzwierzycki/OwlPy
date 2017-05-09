2d1211cd2ac7de47e4ede63f0e85e617edb37db8
Tang Li Qun <tliqun@gmail.com>
2017-05-09 21:55:15 +0900

    some safety measures and value conversion to parse_incoming()
    1. made the while loop break if it reads either an empty line or the end_at_message
    2. made the while loop break if it loops more than 20 times (arbitrary limit, not sure if needed)
    3. check for and turn integers into integers, floats into floats, and the rest stay as strings

--- owl_network/command_parser.py
+++ owl_network/command_parser.py
@@ -3,14 +3,41 @@ import sys
 
 def parse_incoming(end_at_message):
     output_dict = {}
-
+    count = 0
+    
+    def is_int(value):
+        try:
+            int(value)
+            return True
+        except:
+            return False
+    
+    def is_float(value):
+        try:
+            float(value)
+            return True
+        except:
+            return False
+    
     while True:
         current_line = sys.stdin.readline()
         current_line = current_line.strip()
+        if current_line == '':
+            # breaks if it reads an empty line OR finds the end_at_message
+            break
         if current_line != end_at_message:
             current_line = current_line.split(" ")
             print(current_line)
-            output_dict[current_line[0]] = current_line[1]
+            if is_int(current_line[1]) == True:
+                output_dict[current_line[0]] = current_line[1]
+            if is_float(current_line[1]) == True:
+                output_dict[current_line[0]] = current_line[1]
+            else:
+                output_dict[current_line[0]] = current_line[1]
+            count += 1
+        elif count >= 20:
+            # arbitrary limit of 20 max lines
+            break
         else:
             break
 
